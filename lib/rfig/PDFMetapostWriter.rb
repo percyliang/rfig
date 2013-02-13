768cafaa8510914638934a0b5f31255c46e0dcf7
Gabor Angeli <angeli@cs.stanford.edu>
2013-02-12 23:54:47 -0800

    Path independent search for mpto1pdf

--- lib/rfig/PDFMetapostWriter.rb
+++ lib/rfig/PDFMetapostWriter.rb
@@ -14,7 +14,9 @@ class PDFMetapostWriter < MetapostWriter
     logPath = mpPath.sub(/\.mp$/, '.log')
     # Compile the metapost into PDF
     # Don't need the metapost anymore
-    if not ExternalCommand.exec(:command => 'mpto1pdf', :abortIfFail => false,
+    libDir = File.dirname(__FILE__)
+    mpto1pdf = "#{libDir}/../../bin/mpto1pdf"
+    if not ExternalCommand.exec(:command => mpto1pdf, :abortIfFail => false,
         :args => [@verbose ? nil : '-quiet', mpPath].compact) then
       puts "Error converting MP to PDF; see error in #{logPath} (good luck)."
       puts "Also look in #{IO.readlines(logPath)[0].split[-1]}/mpxerr.log"
